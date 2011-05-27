2964b12e37b881f6d2a0443f6ea48e44542b40ba
guillermooo <guilan70@hotmail.com>
2011-05-28 00:05:50 +0200

    New fabfile.

--- fabfile.py
+++ fabfile.py
@@ -3,11 +3,20 @@ from fabric.api import run, cd, env
 env.hosts = ["guillermooo@sublimetext.info"]
 
 def deploy():
-    with cd("hg/sublimehelp"):
-        run("hg pull -u https://bitbucket.org/guillermooo/sublimehelp")
+    with cd("dev/sublimedocs_multilang/es"):
+        run("hg pull -u https://bitbucket.org/guillermooo/sublimehelpes")
         run("rm build -fr")
         run("source ~/venvs/sublimehelp/bin/activate && make html")
 
-    with cd("hg/sublimehelp/build/html"):
-        run("rm ~/webapps/stdocs/* -r")
-        run("cp . ~/webapps/stdocs -r")
+    with cd("dev/sublimedocs_multilang/es/build/html"):
+        run("rm ~/webapps/stdocs/es/* -rf")
+        run("cp . ~/webapps/stdocs/es -r")
+
+    with cd("dev/sublimedocs_multilang/en"):
+        run("hg pull -u https://bitbucket.org/guillermooo/sublimehelpen")
+        run("rm build -fr")
+        run("source ~/venvs/sublimehelp/bin/activate && make html")
+
+    with cd("dev/sublimedocs_multilang/en/build/html"):
+        run("rm ~/webapps/stdocs/en/* -rf")
+        run("cp . ~/webapps/stdocs/en -r")
