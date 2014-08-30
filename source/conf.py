462c593edfe4e8fff9054b2573fabbd4f431841b
FichteFoll <fichtefoll2@googlemail.com>
2014-08-30 04:46:00 +0200

    Check if RTD theme is installed and then use it
    If it's not, the default will be used as previously.

--- source/conf.py
+++ source/conf.py
@@ -93,6 +93,19 @@ exclude_patterns = []
 # a list of builtin themes.
 html_theme = 'default'
 
+# Only import and set the theme if we're building docs locally
+if not os.environ.get('READTHEDOCS', None) == 'True':
+    # Only set the theme if it's available
+    try:
+        import sphinx_rtd_theme
+    except:
+        print("could not find sphinx_rtd_theme, using default")
+    else:
+        html_theme = 'sphinx_rtd_theme'
+        html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
